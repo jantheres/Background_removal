f5157136a91789f72ccb58c70e8e8244f62d69f6
Jan Theres <jantheres31@gmail.com>
2026-04-30 15:48:11 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,10 @@ models = {
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://backgroundremoval-bg-remove.up.railway.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
